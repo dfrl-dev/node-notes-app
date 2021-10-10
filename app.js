492cd63d8ad4373f6177d0d428b26e6dfba21858
Dylan Lauzon <dflauzon@gmail.com>
2021-10-10 13:05:14 -0400

    Implemented command line arguments using yargs

--- app.js
+++ app.js
@@ -1,10 +1,58 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
-const note = getNotes();
+//customize yargs version
+yargs.version('0.0.1');
 
-console.log(note);
+//Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note.',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Title: ' + argv.title)
+        console.log('Body: ' + argv.body)
+    }
 
-console.log(chalk.green("Success!"));
-console.log(chalk.bold.red.inverse("Nodemon is working!"));
+})
 
+//Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note.',
+    handler: function () {
+        console.log('Removing a note!')
+    }
+})
+
+ //Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List all notes',
+    handler: function() {
+        console.log('Listing all notes')
+    }
+})
+
+ //Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Read a note')
+    }
+})
+
+yargs.parse();
